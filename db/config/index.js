cbf040de5e48110b44f2ebe7d344d02bf2cbb58e
MankanTest <102286492+MankanTest@users.noreply.github.com>
2022-04-26 18:31:43 +0400

    Update index.js

--- db/config/index.js
+++ db/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
  // POSTGRES: 'postgres://postgres:post@134.0.119.110:5432/postgres',
    POSTGRES: 'postgres://postgres:post@localhost:5432/postgres',
-  REDIS_URL: process.env.REDIS_URL || 'redis://host.docker.internal:6379',
+  REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379',
 };
